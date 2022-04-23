5148f73935d6eb4bf6879c7e36ea19d0d6e2546c
emdsteph <emdsteph@ucsc.edu>
2022-04-22 21:01:07 -0700

    Control rocket after fired

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -11,13 +11,12 @@ class Rocket extends Phaser.GameObjects.Sprite {
 
     update() {
         // Left/right movement
-        if(!this.isFiring) {
-            if(keyLEFT.isDown && this.x >= borderUISize + this.width) {
-                this.x -= this.moveSpeed;
-            } else if (keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width) {
-                this.x += this.moveSpeed;
-            }
+        if(keyLEFT.isDown && this.x >= borderUISize + this.width) {
+            this.x -= this.moveSpeed;
+        } else if (keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width) {
+            this.x += this.moveSpeed;
         }
+        
         // Fire button
         if(Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
             this.isFiring = true;
